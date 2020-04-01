fedd0c8e440b63e1f76b5ba0f0531d3ba917e4a2
youtingkun <997937187@qq.com>
2020-04-02 00:48:38 +0800

    feat: 1

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function npmDemo(argument) {
-  var name = "finit";
+  var name = 'finit'
   var f1 = function f(arg) {
-    console.log(arg);
-  };
+    console.log(arg, '新的方法')
+  }
   return {
     name: name,
     f1: f1
-  };
+  }
 }
-module.exports = npmDemo();
+module.exports = npmDemo()
