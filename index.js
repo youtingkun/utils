1616677184a89336bd505417f986f0f7f4ed0507
youtingkun <youtingkun@techsailing.com>
2021-06-05 09:51:57 +0800

    feat: version demo

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ export default {
   Object,
 };
 
-//  Git commit之后，运行npm version patch,就可以添加新版本，并把记录添加到CHANGELOG.md里面。并推送到远程仓库并发布。
+//  Git commit之后，运行npm version patch,就可以添加新版本，并把记录添加到CHANGELOG.md里面。并推送到远程仓库并发布。比如npm version v1.0.0
 // 如果不需要发布，和普通操作一样，commit之后直接push就行
