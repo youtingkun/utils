2f450e56317bfa876beac303f5c289efc7d885f3
youtingkun <youtingkun@techsailing.com>
2021-06-05 09:54:21 +0800

    feat: treeTolist

--- modules/Object.js
+++ modules/Object.js
@@ -12,16 +12,28 @@
 // traverse(a)
 
 // 遍历对象
-const  traverse=function(o){
-    for (let i in o) {
-        if (!!o[i] && typeof (o[i]) === "object") {
-            console.log(i, o[i]);
-            traverse(o[i]);
-        }
+const traverse = function (o) {
+  for (let i in o) {
+    if (!!o[i] && typeof o[i] === "object") {
+      console.log(i, o[i]);
+      traverse(o[i]);
     }
-}
+  }
+};
 
+const treeToList = function (data) {
+  let res = [];
+  const dfs = (tree) => {
+    tree.forEach((item) => {
+      if (item.children) {
+        dfs(item.children);
+        delete item.children;
+      }
+      res.push(item);
+    });
+  };
+  dfs(data);
+  return res;
+};
 
-export {
-    traverse
-}
+export { traverse, treeToList };
