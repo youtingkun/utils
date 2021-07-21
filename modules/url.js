d03c59cd41e3ec4f6e3f968f1eb59095cd08c9a6
youtingkun <youtingkun@techsailing.com>
2021-07-21 20:53:20 +0800

    v1.0.26

--- modules/url.js
+++ modules/url.js
@@ -1,7 +1,16 @@
 // 获取url的参数
-export const getQueryString = (name) => {
-  const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-  const search = window.location.search.split("?")[1] || "";
-  const r = search.match(reg) || [];
-  return r[2];
+export const getQueryString = () => {
+  let params = {};
+  const searchUrl =
+    window.location.search.split("?")[1] ||
+    window.location.hash.split("?")[1] ||
+    "";
+  const paramList = searchUrl.split("&");
+  for (let i = 0; i < paramList; i++) {
+    let q = query[i].split("=");
+    if (q.length === 2) {
+      params[decodeURIComponent(q[0])] = decodeURIComponent(q[1]);
+    }
+  }
+  return params;
 };
