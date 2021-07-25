0b279d57c0da8ce9d9ae46028cbe615e8838bfb8
youtingkun <youtingkun@163.com>
2021-07-26 01:49:39 +0800

    feat: 修改导出方式

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 export { getQueryString } from "./modules/url";
 export { arrayToObjectTree } from "./modules/Array";
-import test from "./modules/test.js";
-import RegExpRule from "./modules/RegExpRule";
-import Object from "./modules/Object";
-
-export default {
-  test,
-  RegExpRule,
-  Object,
-};
+export {
+  isString,
+  isNumber,
+  isBoolean,
+  isFunction,
+  isNull,
+  isUndefined,
+  isObject,
+  isArray,
+  isDate,
+  isRegExp,
+  isError,
+  isSymbol,
+  isPromise,
+  isSet,
+} from "./modules/typeRule";
 
 //  Git commit之后，运行npm version patch,就可以添加新版本，并把记录添加到CHANGELOG.md里面。并推送到远程仓库并发布。比如npm version v1.0.0
 // 如果不需要发布，和普通操作一样，commit之后直接push就行
