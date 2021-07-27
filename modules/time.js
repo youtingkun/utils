57c35900dc4a66faeda2884665d027584bcd2341
youtingkun <youtingkun@techsailing.com>
2021-07-27 09:59:00 +0800

    feat: windows

--- modules/time.js
+++ modules/time.js
@@ -2,17 +2,16 @@
 与时间相关的方法
  */
 
-export const countDown= function (firstTime,lastTime){
-    const offsetTime = lastTime - firstTime
-    const day = parseInt(offsetTime/(60*60*24*1000))
-    const hour = parseInt(offsetTime/(60*60*1000)%24)
-    const minu = parseInt(offsetTime/(60*1000)%60)
-    const sec = parseInt(offsetTime/1000%60)
-   return {
-       day,
-       hour,
-       minu,
-       sec
-   }
-}
-
+export const countDown = function (firstTime, lastTime) {
+  const offsetTime = lastTime - firstTime;
+  const day = parseInt(offsetTime / (60 * 60 * 24 * 1000));
+  const hour = parseInt((offsetTime / (60 * 60 * 1000)) % 24);
+  const minute = parseInt((offsetTime / (60 * 1000)) % 60);
+  const second = parseInt((offsetTime / 1000) % 60);
+  return {
+    day,
+    hour,
+    minute,
+    second,
+  };
+};
