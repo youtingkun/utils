ff5de371533ee506ad2fa67289efbeb8a8a3f496
youtingkun <youtingkun@techsailing.com>
2021-08-11 20:53:50 +0800

    feat: number

---
+++ modules/number.js
@@ -0,0 +1,12 @@
+// 求两个数的最大公约数
+//思想是采用辗转相除的方法，用大的数去除以小的那个数，然后再用小的数去除以的得到的 余数，一直这样递归下去，直到余数为 0 时，最后的被除数就是两个数的最大公约数。
+export function getMaxCommonDivisor(a, b) {
+  if (b === 0) return a;
+  return getMaxCommonDivisor(b, a % b);
+}
+
+// 求两个数的最小公倍数
+// 思想是采用将两个数相乘，然后除以它们的最大公约数
+export function getMinCommonMultiple(a, b) {
+  return (a * b) / getMaxCommonDivisor(a, b);
+}
