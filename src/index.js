3e08f551daf32177ddf2658f69298524c63a97b7
youtingkun <youtingkun@techsailing.com>
2021-09-07 14:06:24 +0800

    feat: webpack

--- src/index.js
+++ src/index.js
@@ -38,5 +38,6 @@ export {
   isPositive,
 } from "./modules/regexp-rule";
 
+//  npm run build 先在本地build好打包之后的文件
 //  Git commit之后，运行npm version v1.0.0,就可以添加新版本，并把记录添加到CHANGELOG.md里面,并推送到github，并自动发布到npm。
 // 如果推送到github时网络超时，重新运行 npm run postversion
