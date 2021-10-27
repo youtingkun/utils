486a5be653f8079437d113fea2f10196a5797fdf
youtingkun <youtingkun@163.com>
2021-10-27 16:52:40 +0800

    feat: url

--- src/modules/number.ts
+++ src/modules/number.ts
@@ -161,3 +161,11 @@ export const changeToChinese = (Num) => {
   }
   return newchar;
 };
+
+// 转化为千位分隔符
+export function parseToMoney(num) {
+    num = parseFloat(num.toFixed(3));
+    let [integer, decimal] = String.prototype.split.call(num, '.');
+    integer = integer.replace(/\d(?=(\d{3})+$)/g, '$&,');
+    return integer + '.' + (decimal ? decimal : '');
+}
